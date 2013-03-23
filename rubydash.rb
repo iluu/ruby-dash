7a260bbf86704b10b262fc26b8f02d59fd7ac9d4
iluu <kru@gmail.com>
2013-03-23 19:35:14 +0100

    Updated menu

--- rubydash.rb
+++ rubydash.rb
@@ -15,8 +15,6 @@ def show_logo
   case inp.chomp
     when 'S'
       run_game
-    when 'B'
-      show_best_scores
     when 'E'
       exit
     else
@@ -28,11 +26,6 @@ def prompt(*args)
   print(*args)
   gets
 end
-
-def show_best_scores
-  puts 'No scores yet :('
-end
-
 
 def run_game
   Gaminator::Runner.new(RubydashGame).run
